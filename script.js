2f765b753e174893530c171d65f77c95b0b74dcd
Javid Magsudov <javidmaksudov37@gmail.com>
2024-08-28 00:45:00 +0400

    Almost Ready
    Responsive not done

--- script.js
+++ script.js
@@ -31,3 +31,26 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     });
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+
+        breakpoints: {
+            0:{
+                slidesPerView: 1
+            },
+            620:{
+                slidesPerView: 2
+            },
+            1024:{
+                slidesPerView: 3
+            }
+        }
+      });
